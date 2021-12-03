c9e51c9c86be09516e36aea1ead0a3de6b68bdd7
Jonathan Borofsky <jonborofsky@gmail.com>
2021-12-03 14:15:37 -0600

    db

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,6 +5,18 @@ const knex = require("./db/connection");
 
 app.listen(PORT, listener);
 
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+
+
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
 }
